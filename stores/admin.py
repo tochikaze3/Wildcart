5fdeb84503dd69776e83bb2eee2b0d0205c483b8
tochukwu <tochikaze3@gmail.com>
2022-02-09 19:23:16 +0100

    added new product image admin

--- stores/admin.py
+++ stores/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import Products, Vendor, Category
+from .models import Products, Vendor, Category, ProductImage
 # Register your models here.
 
+
+from .models import ProductImage
+ 
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+ 
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
+ 
+    class Meta:
+       model = Products
+ 
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Products)
 admin.site.register(Vendor)
