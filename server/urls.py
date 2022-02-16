1f21a9de1f5e5287fd2e478adcc71c89e33eaf8f
tochukwu <tochikaze3@gmail.com>
2022-02-16 03:39:53 +0100

    added mew changes

--- server/urls.py
+++ server/urls.py
@@ -17,18 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls 
-<<<<<<< HEAD
-from rest_framework.schemas import get_schema_view
-from django.urls import path, include
-
-schema_view = get_schema_view(title='wildcart')
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='wildcart')),
-    path('api/', include('api.url')),
-    path('schema/', schema_view),
-=======
 from rest_framework.schemas import get_schema_view 
 from django.urls import path, include
 
@@ -42,5 +30,4 @@ urlpatterns = [
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
      path('schema/', schema_view), 
->>>>>>> test
 ]
