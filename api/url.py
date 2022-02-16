99827d4917af7e4c102356d2db6bf58c63567902
tochukwu <tochikaze3@gmail.com>
2022-02-16 03:51:03 +0100

    aceptred incomming changes for recent merge

--- api/url.py
+++ api/url.py
@@ -7,16 +7,9 @@ from rest_framework.routers import SimpleRouter
 
 
 router = SimpleRouter()
-<<<<<<< HEAD
-router.register('vendors', VendorViewSet, basename= 'vendors') 
-router.register('products', ProductViewSet, basename= 'products')
-router.register('productimages/', ProductImageViewSet, basename= 'productimages')
-router.register('categories/', CategoryViewSet, basename= 'categories')
-=======
 router.register('vendors', VendorViewSet, basename='vendors') 
 router.register('products', ProductViewSet, basename='products')
 router.register('productimages', ProductImageViewSet, basename='productimages')
 router.register('categories', CategoryViewSet, basename='categories')
->>>>>>> test
 
 urlpatterns = router.urls
