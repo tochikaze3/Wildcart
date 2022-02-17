0c2ad10891094ce7757e5e5de99e49b1c179f801
tochukwu <tochikaze3@gmail.com>
2022-02-17 15:53:51 +0100

    added recent migrations

--- server/urls.py
+++ server/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from api.url import router
 from rest_framework.documentation import include_docs_urls 
 from rest_framework.schemas import get_schema_view 
@@ -22,7 +22,7 @@ from django.urls import path, include
 schema_view = get_schema_view(title='wildcart') 
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
+   #path('admin/', admin.site.urls),
     path('docs/', include_docs_urls(title='wildcart')), 
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')), 
