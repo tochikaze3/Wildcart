2fa579dc44f09116bc248dc17250997ba175a24b
tochukwu <tochikaze3@gmail.com>
2022-02-17 19:01:32 +0100

    added customized autrhentication

--- server/settings.py
+++ server/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['wildcart.herokuapp.com', '0.0.0.0', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
