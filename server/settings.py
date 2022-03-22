67e69e64b8456af2933112c0b2f6406464ab10f2
tochukwu <tochikaze3@gmail.com>
2022-03-22 10:24:38 +0100

    added new models

--- server/settings.py
+++ server/settings.py
@@ -201,7 +201,7 @@ STATIC_URL = '/staticfiles/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+#MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- stores/models.py
+++ stores/models.py
@@ -1,20 +1,25 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 from django.utils import timezone
 # Create your models here.
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
 
+    class Meta:
+        ordering = ('name',)
+    
     def __str__(self):
-    	return self.category
-
-
-@staticmethod
-def get_all_categories():
-    return Category.objects.all()
-
-
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+        
 class Vendor(models.Model):
     #user = models.OneToOneField(UserProfile, related_name='vendor', on_delete=models.CASCADE)
     store_name = models.CharField(help_text= 'Your store name', default= '', max_length= 250)
@@ -30,9 +35,12 @@ class Vendor(models.Model):
 class Products(models.Model):
     product_name = models.CharField(max_length=100, default = '')
     #store_name = models.ForeignKey(Vendor, default = '', on_delete= models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField()
     upload_Product_Image = models.ImageField(default='default.jpg', upload_to = 'products')
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default = '')
-    price = models.IntegerField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default = '')       
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)                                  
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     Size_Status = (
             ('X', 'Small'),
             ('XX', 'Large'),
@@ -49,6 +57,42 @@ class Products(models.Model):
     class Meta:
         ordering = ['-date_created']
 
+       
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return 'http://127.0.0.1:8000' + self.thumbnail.url
+            else:
+                return ''
+    
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
     def __str__(self):
         return self.product_name
 
@@ -58,6 +102,7 @@ class ProductImage(models.Model):
  
     def __str__(self):
         return self.product.product_name
+
 
 
 class Services(models.Model):
