3628af996ba83a5120c0fac08efdf17976b76e72
tochikaze3 <tochikaze3@gmail.com>
2022-04-14 07:55:07 +0100

    added new models

--- stores/models.py
+++ stores/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.utils import timezone
 # Create your models here.
 
 
@@ -13,28 +14,34 @@ class Category(models.Model):
 @staticmethod
 def get_all_categories():
     return Category.objects.all()
+ 
 
 
 class Products(models.Model):
-        id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-        product_Name = models.CharField(max_length=250, help_text = 'Enter product name' )
-        upload_Product_Image = models.ImageField(default='default.jpg')
-        product_Description = models.CharField(default = '', max_length= 200)
-        weight = models.CharField(max_length= 100, default = 'kg', blank= True)
-
-        Size_Status = (
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Product_name = models.CharField(max_length=100, default = '')
+    #store_name = models.ForeignKey(Vendor, default = '', on_delete= models.CASCADE)
+    upload_Product_Image = models.ImageField(default='default.jpg', upload_to = 'staticfiles/products')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default = '')
+    price = models.IntegerField()
+    Size_Status = (
             ('X', 'Small'),
             ('XX', 'Large'),
             ('XXL', 'Extra large'),
             )
 
-        size = models.CharField(max_length=5, choices= Size_Status, blank= True, default='X')
-        stock = models.IntegerField(default= 'Choose total number of goods available')
-        price = models.DecimalField(max_digits=1000,  default= 00.00, decimal_places=2)
-                
-        
-        def __str__(self):
-            return self.product_Name
+    size = models.CharField(max_length=5, choices= Size_Status, blank= True, default='X')
+    stock = models.IntegerField()
+    #imageUrl = models.URLField(default='')
+   #created_by = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return '{} {}'.format(self.Product_name)
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Products, default=None, on_delete=models.CASCADE)
