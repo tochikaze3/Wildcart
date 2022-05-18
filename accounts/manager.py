d658d1f27ce6de498b817a60ca4c626fd689a738
tochikaze3 <tochikaze3@gmail.com>
2022-05-18 09:00:35 +0100

    added new folders

---
+++ accounts/manager.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+
+
+class AccountManager(BaseUserManager):
+    
+    def create_account(self, email, username, password=None):
+       if not email:
+           raise ValueError("Users must have an email address")
+       if not username: 
+           raise ValueError("Users must have a username")
+       user = self.model(
+               email= self.normalize_email(email),
+               username=username,
+        )
+       user.set_password(password)
+       user.save(using=self._db)
+       return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email= self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
---
+++ accounts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from .manager import AccountManager
+from django.contrib.auth.models import AbstractBaseUser 
+# Create your models here.
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(verbose_name= "Email", max_length= 50, null=False, Unique = True)
+    username = models.CharField(max_length= 50, unique= True)
+    date_joined = models.DateTimeField(verbose_name= "Date Joined", auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name= "Last Login", auto_now=True)
+    is_admin = models.BooleanField(default = False)
+    is_staff = models.BooleanField(default= False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default= False)
+    hide_email = models.BooleanField(default= True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS= ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
