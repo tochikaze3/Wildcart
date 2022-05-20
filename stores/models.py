002b7a37d8a6330d6a5c16c573ec41431b2d8b20
tochikaze3 <tochikaze3@gmail.com>
2022-05-20 18:53:49 +0100

    completed changes

--- stores/models.py
+++ stores/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django.utils import timezone
+from accounts.models import Account
 # Create your models here.
 
 
@@ -16,11 +17,22 @@ def get_all_categories():
     return Category.objects.all()
  
 
+class Vendor(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE, default="")
+    store_name = models.CharField(help_text= 'Your store name', default= '', max_length= 250)
+    logo = models.ImageField(upload_to = 'staticfiles/images')
+    about = models.TextField(max_length=1000, help_text='Give a catchy description of your store', default= '')
+    phone = models.CharField(max_length=20, default= "", null= False, blank= False)
+    email = models.EmailField(default= '', max_length= 250, help_text= 'Your email name')
+    location = models.CharField(help_text= 'Enter your store location', default= '', max_length= 250)
+
+    def __str__(self):       
+        return self.store_name
 
 class Products(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     Product_name = models.CharField(max_length=100, default = '')
-    #store_name = models.ForeignKey(Vendor, default = '', on_delete= models.CASCADE)
+    store_name = models.ForeignKey(Vendor, default = '', on_delete= models.CASCADE)
     upload_Product_Image = models.ImageField(default='default.jpg', upload_to = 'staticfiles/products')
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default = '')
     price = models.IntegerField()
@@ -33,7 +45,7 @@ class Products(models.Model):
     size = models.CharField(max_length=5, choices= Size_Status, blank= True, default='X')
     stock = models.IntegerField()
     #imageUrl = models.URLField(default='')
-   #created_by = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
+    #created_by = models.ForeignKey(Vendor, related_name='products', on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
     date_created = models.DateField(default=timezone.now)
 
@@ -51,17 +63,6 @@ class ProductImage(models.Model):
     def __str__(self):
         return self.product.product_Name
 
-class Vendor(models.Model):
-    #user = models.OneToOneField(UserProfile, related_name='vendor', on_delete=models.CASCADE)
-    store_name = models.CharField(help_text= 'Your store name', default= '', max_length= 250)
-    logo = models.ImageField(upload_to = 'staticfiles/images')
-    about = models.TextField(max_length=1000, help_text='Give a catchy description of your store', default= '')
-    phone = models.CharField(max_length=20, default= "", null= False, blank= False)
-    email = models.EmailField(default= '', max_length= 250, help_text= 'Your email name')
-    location = models.CharField(help_text= 'Enter your store location', default= '', max_length= 250)
-
-    def __str__(self):       
-        return self.store_name
 
 class Services(models.Model):
     service = models.ForeignKey(Vendor, default='', on_delete= models.CASCADE)
