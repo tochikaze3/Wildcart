80a381e106e42c0f1034266f9327a4583adc1194
tochikaze3 <tochikaze3@gmail.com>
2022-05-21 07:57:23 +0100

    removed username variables

--- accounts/manager.py
+++ accounts/manager.py
@@ -5,23 +5,19 @@ from django.contrib.auth.models import BaseUserManager
 
 class AccountManager(BaseUserManager):
     
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None):
        if not email:
            raise ValueError("Users must have an email address")
-       if not username: 
-           raise ValueError("Users must have a username")
        user = self.model(
                email= self.normalize_email(email),
-               username=username,
         )
        user.set_password(password)
        user.save(using=self._db)
        return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, email, password):
         user = self.create_user(
             email= self.normalize_email(email),
-            username=username,
             password=password,
         )
         user.is_admin = True
--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from .manager import AccountManager
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 # Create your models here.
 
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name= "Email", max_length= 50, null=False, unique = True)
-    username = models.CharField(max_length= 50, unique= True)
+    #username = models.CharField(max_length= 50, unique= True)
     date_joined = models.DateTimeField(verbose_name= "Date Joined", auto_now_add=True)
     last_login = models.DateTimeField(verbose_name= "Last Login", auto_now=True)
     is_admin = models.BooleanField(default = False)
@@ -17,10 +17,10 @@ class Account(AbstractBaseUser, PermissionsMixin):
     objects = AccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS= ['username']
+   # REQUIRED_FIELDS= ['username']
 
     def __str__(self):
-        return self.username
+        return self.email
 
     def has_perm(self, obj=AccountManager()):
 
