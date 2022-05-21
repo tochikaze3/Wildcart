23ecf99b9bacfe505ce033e151d22b15cb0d34a8
tochikaze3 <tochikaze3@gmail.com>
2022-05-21 07:59:23 +0100

    added username variables

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class Account(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name= "Email", max_length= 50, null=False, unique = True)
-    #username = models.CharField(max_length= 50, unique= True)
+    username = models.CharField(max_length= 50, unique= True)
     date_joined = models.DateTimeField(verbose_name= "Date Joined", auto_now_add=True)
     last_login = models.DateTimeField(verbose_name= "Last Login", auto_now=True)
     is_admin = models.BooleanField(default = False)
@@ -17,10 +17,10 @@ class Account(AbstractBaseUser, PermissionsMixin):
     objects = AccountManager()
 
     USERNAME_FIELD = 'email'
-   # REQUIRED_FIELDS= ['username']
+    REQUIRED_FIELDS= ['username']
 
     def __str__(self):
-        return self.email
+        return self.username
 
     def has_perm(self, obj=AccountManager()):
 
